0bdfb52fed1ce8d42a5bed6ee6697d67faa37f4a
KurisuM <mkurisuqwq@icloud.com>
2025-09-01 20:58:36 +0800

    25/09/01

---
+++ P7230.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> pii;
+
+const int MAXN = 1e5 + 10;
+int n, m, q, top, a[MAXN], f[MAXN], ans[MAXN];
+bitset<MAXN> vis;
+multiset<int> st[MAXN];
+
+namespace sgt {
+    struct Node {
+        int l, r, res, mn;
+    };
+} // namespace sgt
+
+signed main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0), cout.tie(0);
+
+    /* code */
+
+    return 0;
+}
