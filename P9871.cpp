fd55b86e44fc289560002694234b5be5478dd063
MKurisu <mkurisuqwq@icloud.com>
2025-10-28 22:22:52 +0800

    25/10/28

--- P9871.cpp
+++ P9871.cpp
@@ -1,4 +1,5 @@
 #include <bits/stdc++.h>
+#define int long long
 #define lc(p) p << 1
 #define rc(p) p << 1 | 1
 #define lson lc(p), l, mid
@@ -36,7 +37,7 @@ void build(int p, int l, int r) {
 }
 
 void modify(int p, int l, int r, int x, int y, int val) {
-    if (l <= x && y <= r) return t[p].entag(val);
+    if (x <= l && r <= y) return t[p].entag(val);
     pushdown(p);
     int mid = (l + r) >> 1;
     if (x <= mid) modify(lson, x, y, val);
@@ -45,9 +46,10 @@ void modify(int p, int l, int r, int x, int y, int val) {
 }
 
 int query(int p, int l, int r, int x, int y) {
-    if (l <= x && y <= r) return t[p].val;
+    if (x > y) return 0;
+    if (x <= l && r <= y) return t[p].val;
     pushdown(p);
-    int mid = (l + r) > 1;
+    int mid = (l + r) >> 1;
     if (y <= mid) return query(lson, x, y);
     if (x > mid) return query(rson, x, y);
     return max(query(lson, x, y), query(rson, x, y));
@@ -68,27 +70,30 @@ void solve() {
         num[++cnt] = x - y;
         num[++cnt] = x;
     }
+
     sort(num + 1, num + 1 + cnt);
     cnt = unique(num + 1, num + 1 + cnt) - num - 1;
-    build(1, 1, cnt);
-    for (int i = 1; i <= m; i++) {
-        a[i].l = lower_bound(num + 1, num + 1 + cnt, a[i].l) - num;
-        a[i].r = lower_bound(num + 1, num + 1 + cnt, a[i].r) - num;
-    }
+    auto id = [=](int x) { return lower_bound(num + 1, num + 1 + cnt, x) - num; };
+    for (int i = 1; i <= m; i++) a[i].l = id(a[i].l), a[i].r = id(a[i].r);
     sort(a + 1, a + 1 + m);
-    int ans = 0, p = 1;
-    for (int i = 1; i <= cnt; i++) {
-        modify(1, 1, cnt, 1, i, -d * (num[i] - num[i - 1]));
-        for (; p <= m && a[p].r == i; p++) modify(1, 1, cnt, 1, a[p].l + 1, a[p].val);
+
+    int ans = 0;
+    build(1, 0, cnt);
+    for (int i = 1, j = 1; i <= cnt; i++) {
+        modify(1, 0, cnt, 0, i - 1, -d * (num[i] - num[i - 1]));
+        for (; j <= m && a[j].r <= i; j++) modify(1, 0, cnt, 0, a[j].l, a[j].val);
+        ans = max(ans, query(1, 0, cnt, id(num[i] - k), i - 1));
+        if (i + 1 <= cnt) modify(1, 0, cnt, i + 1, i + 1, ans);
     }
+    cout << ans << '\n';
 }
 
 signed main() {
     ios::sync_with_stdio(0);
     cin.tie(0), cout.tie(0);
 
-    int T;
-    cin >> T;
+    int T, id;
+    cin >> id >> T;
     while (T-- > 0) solve();
 
     return 0;
